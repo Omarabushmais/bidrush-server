15135fe01375396560a74a98c7ed815f6ca4ce64
Omar <Omarabushmais@gmail.com>
2026-01-22 19:54:25 +0300

    added another upload type

--- middleware/upload.js
+++ middleware/upload.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  const filetypes = /jpeg|jpg|png/;
+  const filetypes = /jpeg|jpg|png|webp/;
   
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
